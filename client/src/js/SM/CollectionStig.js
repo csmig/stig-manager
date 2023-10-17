439ea5ef91eac58b03c197e466e8e0dbdd34a05c
csmig <csmig@csmig.com>
2023-10-17 15:56:36 -0400

    handle new STIG operation

--- client/src/js/SM/CollectionStig.js
+++ client/src/js/SM/CollectionStig.js
@@ -568,6 +568,7 @@ SM.CollectionStigProperties = Ext.extend(Ext.form.FormPanel, {
                     const revisions = [['latest', 'Most recent revision'], ...record.data.revisions.map( rev => [rev.revisionStr, `${rev.revisionStr} (${rev.benchmarkDate})`])]
                     revisionComboBox.store.loadData(revisions)
                     revisionComboBox.setValue(record.data.benchmarkId === _this.benchmarkId ? _this.defaultRevisionStr : 'latest')
+                    assetSelectionPanel.trackedProperty = { dataProperty: 'benchmarkIds', value: record.data.benchmarkId }
                     setButtonState()
                 }
             }
